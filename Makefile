59ff0e42925a0e063c7ed13aa63fa303c67b185c
watanabe.ryuichi <jpz2046@linecorp.com>
2019-10-15 16:21:46 +0900

    first commit

--- Makefile
+++ Makefile
@@ -2,23 +2,38 @@ DOCKER_COMPOSE := $(shell which docker-compose)
 
 pass:
 	:
-build:
-	${DOCKER_COMPOSE} down --rmi all
-	${DOCKER_COMPOSE} up -d
 
-restart:
-	${DOCKER_COMPOSE} down
-	${DOCKER_COMPOSE} up -d
+.PHONY: list
+list:
+	date ; ${DOCKER_COMPOSE} ps
 
-poc:
-	${DOCKER_COMPOSE} exec poc bash
+up:
+	date ; ${DOCKER_COMPOSE} up -d
 
+down:
+	date ; ${DOCKER_COMPOSE} down
+
+.PHONY: build
+build: list
+	date ; ${DOCKER_COMPOSE} down --rmi all
+	date ; ${DOCKER_COMPOSE} up -d
+
+.PHONY: restart
+restart: list down up
+
+.PHONY: pocs
+pocs:
+	date ; ${DOCKER_COMPOSE} exec poc bash
+
+.PHONY: nginx
 nginx:
-	${DOCKER_COMPOSE} exec nginx001 bash
+	date ; ${DOCKER_COMPOSE} exec nginx001 bash
 
+.PHONY: reload
 reload:
-	${DOCKER_COMPOSE} exec nginx001 nginx -t
-	${DOCKER_COMPOSE} exec nginx001 nginx -s reload
+	date ; ${DOCKER_COMPOSE} exec nginx001 nginx -t
+	date ; ${DOCKER_COMPOSE} exec nginx001 nginx -s reload
 
+.PHONY: web01
 web01:
-	${DOCKER_COMPOSE} exec webapp001 bash
+	date ; ${DOCKER_COMPOSE} exec webapp001 bash
--- webapp/main.py
+++ webapp/main.py
@@ -6,49 +6,68 @@ import datetime
 import os
 import socket
 import sys
+import time
 import functools
 
-from flask import Flask, request, make_response, jsonify
+from flask import Flask, request, make_response, jsonify, render_template
 from multiprocessing import Pool, Process, cpu_count
 
 app = Flask(__name__)
 
-@app.route('/_info', methods=['GET'])
+
+@app.route("/_info", methods=["GET"])
 def index():
     return jsonify(
         {
-            "headers" : dict(request.headers),
+            "headers": dict(request.headers),
             "host": {
-                "name" : os.uname()[1],
-                "ip" : socket.gethostbyname(socket.gethostname()),
-                "ppid" : os.getppid(),
-                "pid" : os.getpid()
-            }
+                "name": os.uname()[1],
+                "ip": socket.gethostbyname(socket.gethostname()),
+                "ppid": os.getppid(),
+                "pid": os.getpid(),
+            },
         }
     )
 
-@app.route('/status/<int:status_code>', methods=['GET'])
+
+@app.route("/test/status/<int:status_code>", methods=["GET"])
 def return_http_status(status_code):
     status_list = {
         200: "OK",
         400: "Bad Request",
         404: "Not Found",
-        500: "Internal Server Error"
+        500: "Internal Server Error",
     }
 
     try:
         status_list[status_code]
     except KeyError:
-        return jsonify({"status" : status_list[404]}), 404
+        return jsonify({"status": status_list[404]}), 404
 
-    return jsonify({"status" : status_list[status_code]}), status_code
+    return jsonify({"status": status_list[status_code]}), status_code
 
-if __name__ == '__main__':
-    CRUD_JSON_AS_ASCII = os.getenv('JSON_AS_ASCII') or False
-    CRUD_FLASK_DEBUG_MODE = os.getenv('FLASK_DEBUG_MODE') or True
-    CRUD_FLASK_IP_ADDR = os.getenv('FLASK_IP_ADDR') or "0.0.0.0"
-    CRUD_FLASK_PORT = os.getenv('FLASK_PORE_NUM') or 5000
 
-    app.config['JSON_AS_ASCII'] = CRUD_JSON_AS_ASCII
+@app.route("/test/sleep", methods=["GET", "POST"])
+def sleep():
+    if request.method == "GET":
+        time.sleep(int(request.args.get("st", 10)))
+        return request.method + "\n"
+    else:
+        time.sleep(int(request.form["st"]))
+        return "POST"
+
+
+@app.route("/test/static/<string:filename>", methods=["GET"])
+def return_static(filename):
+    return render_template(filename + ".html", message=filename)
+
+
+if __name__ == "__main__":
+    CRUD_JSON_AS_ASCII = os.getenv("JSON_AS_ASCII") or False
+    CRUD_FLASK_DEBUG_MODE = os.getenv("FLASK_DEBUG_MODE") or True
+    CRUD_FLASK_IP_ADDR = os.getenv("FLASK_IP_ADDR") or "0.0.0.0"
+    CRUD_FLASK_PORT = os.getenv("FLASK_PORE_NUM") or 5000
+
+    app.config["JSON_AS_ASCII"] = CRUD_JSON_AS_ASCII
     app.debug = CRUD_FLASK_DEBUG_MODE
     app.run(host=CRUD_FLASK_IP_ADDR, port=CRUD_FLASK_PORT)
